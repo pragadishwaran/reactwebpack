549667415ad7b077fd55c17a7f1b2e14ddbe97be
pragadishwaran <pragadishwaran.baskaran@cesltd.com>
2019-09-03 19:07:29 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path')
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  entry: "./src/index.js", 
+  output: {
+      path: path.resolve("dist"),
+      filename: "bundled.js"
+    },
+    devServer: {
+      port: 9000
+    },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
